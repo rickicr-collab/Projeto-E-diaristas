a0910e983e989a645f1ca1ac92854042456ede64
Ricardo Rosendo <ricardo_rosendo@ymail.com>
2025-10-24 11:18:37 -0300

    first configuration on the project in the file application.properties

---
+++ src/main/java/br/com/rickicollab/e_diarista/EDiaristaApplication.java
@@ -0,0 +1,13 @@
+package br.com.rickicollab.e_diarista;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EDiaristaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EDiaristaApplication.class, args);
+	}
+
+}
