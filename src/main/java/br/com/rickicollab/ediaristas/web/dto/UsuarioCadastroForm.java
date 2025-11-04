2e5f8964de247f4ff343a35b0258ca8a860cc751
Ricardo Rosendo <ricardo_rosendo@ymail.com>
2025-11-04 10:13:52 -0300

    add dependenci mappers for auto-generated our form converters Service and user

---
+++ src/main/java/br/com/rickicollab/ediaristas/web/dto/UsuarioCadastroForm.java
@@ -0,0 +1,36 @@
+package br.com.rickicollab.ediaristas.web.dto;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class UsuarioCadastroForm {
+
+    @NotNull
+    @Size(min = 1, max = 255)
+    private String nomeCompleto;
+
+    @NotNull
+    @Size(min = 3, max = 255)
+    @Email
+    private String email;
+
+    @NotNull
+    @NotEmpty
+    private String senha;
+
+    @NotNull
+    @NotEmpty
+    private String confirmacaoSenha;
+
+
+
+
+}
