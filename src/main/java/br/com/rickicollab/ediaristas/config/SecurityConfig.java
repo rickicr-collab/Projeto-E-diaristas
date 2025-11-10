cea415a5b21ee566a2711a086cd5d5a67b0e8976
Ricardo Rosendo <ricardo_rosendo@ymail.com>
2025-11-10 08:38:06 -0300

    configured criptography for the project, with bcryptpassordEncoder

--- src/main/java/br/com/rickicollab/ediaristas/config/SecurityConfig.java
+++ src/main/java/br/com/rickicollab/ediaristas/config/SecurityConfig.java
@@ -12,7 +12,7 @@ public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception{
-        http.authorizeHttpRequests(auth -> auth.anyRequest().permitAll()).csrf(csrf -> csrf.disable());
+        http.authorizeHttpRequests(auth -> auth.anyRequest().permitAll());
         return http.build();
     }
     
